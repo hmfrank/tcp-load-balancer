addf5f77d11e834558909952dc4836505de44a76
Hannes Frank <hannes.frank@mailbox.tu-dresden.de>
2024-09-09 16:49:11 +0200

    Make server echo

--- examples/server.rs
+++ examples/server.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::io;
-use tokio::net::{TcpListener};
+use tokio::net::{TcpListener, TcpStream};
 
 
 #[tokio::main]
@@ -11,6 +11,25 @@ async fn main() -> io::Result<()> {
     let listener = TcpListener::bind(addr).await?;
 
     loop {
-        let _ = listener.accept().await;
+        match listener.accept().await {
+            Ok((socket, client_address)) => {
+                println!("New connection to {:?}", client_address);
+
+                tokio::spawn(async move {
+                    if let Err(e) = echo(socket).await {
+                        println!("Failed to echo client {:?}. {:?}", client_address, e);
+                    }
+                });
+            }
+            Err(e) => {
+                println!("Failed to accept client. {:?}", e);
+            }
+        };
     }
+}
+
+async fn echo(mut socket: TcpStream) -> io::Result<()> {
+    let (mut rx, mut tx) = socket.split();
+    tokio::io::copy(&mut rx, &mut tx).await?;
+    Ok(())
 }
